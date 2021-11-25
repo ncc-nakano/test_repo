06385d9828b3594d7dad0e524ecafdb997523a4b
ncc-nakano <93506589+ncc-nakano@users.noreply.github.com>
2021-11-25 15:31:00 +0900

    aaa

---
+++ hoge_button.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const hoge_e = React.createElement;
+
+class HogeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hoge: false };
+  }
+
+  render() {
+    if (this.state.hoge) {
+      return this.props.commentID + 'hogehogehgoe hogehoge hoge.';
+    }
+
+    return hoge_e(
+      'button',
+      { onClick: () => this.setState({ hoge: true }) },
+      'Hoge'
+    );
+  }
+}
+
+// const hogeDomContainer = document.querySelector('#hoge_button_container');
+// ReactDOM.render(hoge_button_e(HogeButton), hogeDomContainer);
+
+document.querySelectorAll('.hoge_button_container')
+    .forEach(domContainer => {
+
+    const commentID =  parseInt(domContainer.dataset.commentid, 10);
+    ReactDOM.render(hoge_e(HogeButton, {commentID:commentID}), domContainer);
+    
+});
--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 function test() {
     alert('hoge');
 }
+
+function calculate() {
+    var lat1 = document.getElementById("latitude1").value;
+    var lng1 = document.getElementById("longitude1").value;
+    var lat2 = document.getElementById("latitude2").value;
+    var lng2 = document.getElementById("longitude2").value;
+
+    var radLat1 = lat1 * (Math.PI/180);
+    var radLng1 = lng1 * (Math.PI/180);
+    var radLat2 = lat2 * (Math.PI/180);
+    var radLng2 = lng2 * (Math.PI/180);
+
+    
+
+}
